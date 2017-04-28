845820fd0a8683f8c4225a319831b749099d5c12
mail@ti-bor.nl <mail@ti-bor.nl>
2017-04-29 00:38:34 +0200

    minified polyfill

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
 
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-<script src="https://cdn.polyfill.io/v2/polyfill.js?features=String.prototype.endsWith,String.prototype.startsWith,Element.prototype.prepend,Element.prototype.append,Array.from,Element.prototype.remove,Promise,fetch,HTMLPictureElement"></script>
+<script src="https://cdn.polyfill.io/v2/polyfill.min.js?features=String.prototype.endsWith,String.prototype.startsWith,Element.prototype.prepend,Element.prototype.append,Array.from,Element.prototype.remove,Promise,fetch,HTMLPictureElement"></script>
 
 <?php // We are loading our theme directory style.css by queuing scripts in our functions.php file, 
 	// so if you want to load other stylesheets,
