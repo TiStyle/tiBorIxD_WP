f5e4583abe633d915327614a95ab5b4c28c2d94c
TiStyle <mail@ti-bor.nl>
2017-04-28 21:31:18 +0200

    Added files with upload

---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+/// <binding AfterBuild='default' Clean='clean' />
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+var babel = require('gulp-babel');
+var babelminify = require('gulp-babel-minify');
+var rimraf = require('gulp-rimraf');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+
+var filesToWatch = [
+    "sass/*.scss",
+    "js/*.js" //,
+    //"Components/**/*.scss",
+    //"Components/**/*.js"
+];
+
+var filesToClean = [
+    "wwwroot/css/tiborixd.css",
+    "wwwroot/css/tiborixd.css.map",
+    "wwwroot/js/tiborixd.js",
+    "wwwroot/js/tiborixd.js.map",
+];
+
+gulp.task('default', ['js', 'scss']);
+
+gulp.task('clean', function () {
+    return gulp
+        .src(filesToClean, { read: false })
+        .pipe(rimraf({ force: true }));
+});
+
+gulp.task('js', function () {
+    console.log('js');
+   
+    // return gulp
+    //     .src(['App/js/HtmlExtensions.js', 'App/js/stringUtil.js', 'App/js/fileLoader.js', 'App/js/styleHelper.js', 'App/js/dateUtil.js', 'Components/**/*.js'])
+    //     .pipe(sourcemaps.init())
+    //     .pipe(concat('huiskamer.js'))
+    //     .pipe(babel({ presets: ['es2015'] }))
+    //     //.pipe(babelminify({unsafe:false}))
+    //     .pipe(sourcemaps.write('.'))
+    //     .pipe(gulp.dest('wwwroot/js'));
+});
+
+gulp.task('scss', function () {
+    console.log('scss');
+
+    return gulp
+        .src('sass/tiBorIxD.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({ outputStyle :'compressed'}).on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('wwwroot/css'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch(filesToWatch, ['js', 'scss']);
+});
+
---
+++ page-home_page.php
@@ -0,0 +1,40 @@
+<?php 
+/**
+ * 	Template Name: Main Homepage
+ *
+ *	This page template has a sidebar built into it, 
+ * 	and can be used as a home page, in which case the title will not show up.
+ *
+*/
+get_header(); // This fxn gets the header.php file and renders it ?>
+	<div class="row">
+		<div class="col l12 m12 s12">
+			Hoi
+		</div>
+	</div>
+	<div class="row">
+		<div class="col l4 m6 s5">
+			1
+		</div>
+		<div class="col l4 m6 s7">
+			2
+		</div>
+		<div class="col l4 m12 s12">
+			3
+		</div>
+	</div>
+	<div class="row">
+		<div class="col l1 m1 s4"></div>
+		<div class="col l1 m1 s4"></div>
+		<div class="col l1 m1 s4"></div>
+		<div class="col l1 m1 s4"></div>
+		<div class="col l1 m1 s4"></div>
+		<div class="col l1 m1 s4"></div>
+		<div class="col l1 m1 s4"></div>
+		<div class="col l1 m1 s4"></div>
+		<div class="col l1 m1 s4"></div>
+		<div class="col l1 m1 s4"></div>
+		<div class="col l1 m1 s4"></div>
+		<div class="col l1 m1 s4"></div>
+	</div>
+<?php get_footer(); // This fxn gets the footer.php file and renders it ?>
