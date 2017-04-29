f76e149b233982f8bc2b04879a633787d85a03b3
mail@ti-bor.nl <mail@ti-bor.nl>
2017-04-29 14:39:40 +0200

    Home layout

--- page-home_page.php
+++ page-home_page.php
@@ -6,47 +6,25 @@
 get_header(); // This fxn gets the header.php file and renders it ?>
 	<div class="row">
 		<div class="col l6 m6 s12 offset-l1 offset-m1">
-			Hoi
+			What about design?
 		</div>
+        <div class="col l4 m4 s12">
+            About tiBor text
+        </div>
 	</div>
-	<div class="row">
-		<div class="col l4 m6 s5">
+	<div id="projects" class="row">
+		<div class="item col l3 m6 s12">
 			1
 		</div>
-		<div class="col l4 m6 s7">
+		<div class="item col l3 m6 s12">
 			2
 		</div>
-		<div class="col l4 m12 s12">
+		<div class="item col l3 m6 s12">
 			3
 		</div>
-	</div>
-	<div class="row">
-		<div class="col l1 m1 s4"></div>
-		<div class="col l1 m1 s4"></div>
-		<div class="col l1 m1 s4"></div>
-		<div class="col l1 m1 s4"></div>
-		<div class="col l1 m1 s4"></div>
-		<div class="col l1 m1 s4"></div>
-		<div class="col l1 m1 s4"></div>
-		<div class="col l1 m1 s4"></div>
-		<div class="col l1 m1 s4"></div>
-		<div class="col l1 m1 s4"></div>
-		<div class="col l1 m1 s4"></div>
-		<div class="col l1 m1 s4"></div>
-	</div>
-	<div class="row">
-		<div class="col l6 m4 s3"></div>
-		<div class="col l6 m4 s3"></div>
-		<div class="col l6 m4 s3"></div>
-		<div class="col l6 m4 s3"></div>
-		<div class="col l6 m4 s3"></div>
-		<div class="col l6 m4 s3"></div>
-		<div class="col l6 m4 s3"></div>
-		<div class="col l6 m4 s3"></div>
-		<div class="col l6 m4 s3"></div>
-		<div class="col l6 m4 s3"></div>
-		<div class="col l6 m4 s3"></div>
-		<div class="col l6 m4 s3"></div>
+        <div class="item col l3 m6 s12">
+			4
+		</div>
 	</div>
     
 <?php get_footer(); // This fxn gets the footer.php file and renders it ?>
