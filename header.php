09fd030b19c7275f7af2720b08b26694f71a5fc2
Tibor Dujmovic <mail@ti-bor.nl>
2017-06-14 23:11:09 +0200

    menu changes + header hidden

--- header.php
+++ header.php
@@ -61,9 +61,8 @@
             </div>-->
         </nav><!-- .site-navigation .main-navigation -->
 	</div>
-	<div class="center">
 
-		<div id="brand">
+		<div id="brand" class="center hide">
 			<h1 class="site-title">
 				<a href="<?php echo esc_url( home_url( '/' ) ); // Link to the home page ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); // Title it with the blog name ?>" rel="home"><?php bloginfo( 'name' ); // Display the blog name ?></a>
 			</h1>
@@ -73,7 +72,6 @@
 		</div><!-- /brand -->
 		
 		<div class="clear"></div>
-	</div><!--/container -->
     
     <!--<?php if ( has_post_thumbnail() ) : ?>
         <div class="page-image">
