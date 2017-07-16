1ae74eda6d6cb897ffc43cc194bd1e3bf921c642
Tibor Dujmovic <mail@ti-bor.nl>
2017-07-16 12:24:34 +0200

    .

--- functions.php
+++ functions.php
@@ -101,13 +101,13 @@ add_action( 'widgets_init', 'naked_register_sidebars' );
 function tiborixd_scripts()  { 
 
 	// get the theme directory tiborIxD.css and link to it in the header
-	wp_enqueue_style('tiborIxD.css', get_stylesheet_directory_uri() . '/wwwroot/css/tiborIxD.css');
+	wp_enqueue_style('tiborIxD.css', get_stylesheet_directory_uri() . '/dist/css/tiborIxD.css');
 	
 	// add fitvid
 	wp_enqueue_script( 'tiborIxD-fitvid', get_template_directory_uri() . '/js/jquery.fitvids.js', array( 'jquery' ), TIBORIXD_VERSION, true );
 	
 	// add theme scripts
-	wp_enqueue_script( 'tiborIxD', get_template_directory_uri() . '/wwwroot/js/tiborIxD.js', array(), TIBORIXD_VERSION, true );
+	wp_enqueue_script( 'tiborIxD', get_template_directory_uri() . '/dist/js/tiborIxD.js', array(), TIBORIXD_VERSION, true );
   
 }
 add_action( 'wp_enqueue_scripts', 'tiborixd_scripts' ); // Register this fxn and allow Wordpress to call it automatcally in the header
